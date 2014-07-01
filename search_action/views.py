9ce2d8bae0b6204bc5479052c342399d0023fa36
sogo <sogo@sogoui-MacBook-Pro.local>
2014-07-01 16:33:11 +0900

    2014-06-15, search page

--- search_action/views.py
+++ search_action/views.py
@@ -2,6 +2,22 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 # Create your views here.
 
-# Modify this page to search
-def search_view(request):
+# Modify this page for Search MAIN page
+def searchView(request):
+	return render_to_response('search.html')
+
+# Modify this page for IMAGE Search
+def imageSearch(request):
+	return render_to_response('search.html')
+
+# Modify this page for TEXT based Search
+def textSearch(request):
+	return render_to_response('search.html')
+
+# Modify this page for Movie DNA Search
+def movieSearch(request):
+	return render_to_response('search.html')
+
+# Modify this page for Integrated Search
+def intgSearch(request):
 	return render_to_response('search.html')
